cd13071fa017d8350293b4fa2a5032cc6717f649
WesleySDev <you@example.com>
2026-04-22 19:56:34 -0300

    feat: implementar função de registro de usuário com validação de dados

---
+++ SistemaLoginComPonteiro(simples)/controllers/auth.go
@@ -0,0 +1,24 @@
+package controllers
+
+import (
+	"login-go/database"
+	"login-go/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Register(c *gin.Context) {
+	var user models.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"erro": "Dados iválidos"})
+		return
+	}
+
+	database.USers = append(database.USers, user)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Usuário cadastrado",
+	})
+}
